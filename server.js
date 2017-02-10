fada2f945832b7e59af78a0763babd83180ce6dc
GopalJiGupta <gopaljigupta.17@gmail.com>
2017-02-10 19:35:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,74 +5,9 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-var articalOne={  
-    title: 'gopal first artical',
-    heading: 'my artical',
-    date: '2/2/2017',
-    contain: ` <p>
-                My name is Gopal ji gupta and i am hardworker and selfmotivater.My name is Gopal ji gupta and i am hardworker and selfmotivater.My name is Gopal ji gupta and i am hardworker and selfmotivater.My name is Gopal ji gupta and i am hardworker and selfmotivater.
-            </p>
-            <p>
-                My name is Gopal ji gupta and i am hardworker and selfmotivater.My name is Gopal ji gupta and i am hardworker and selfmotivater.My name is Gopal ji gupta and i am hardworker and selfmotivater.My name is Gopal ji gupta and i am hardworker and selfmotivater.
-            </p>
-            <p>
-                My name is Gopal ji gupta and i am hardworker and selfmotivater.My name is Gopal ji gupta and i am hardworker and selfmotivater.My name is Gopal ji gupta and i am hardworker and selfmotivater.My name is Gopal ji gupta and i am hardworker and selfmotivater.
-            </p>`
-};
-
-function createTemplet(data)
-{
- var title = data.title;
- var contain = data.contain;
- var date = data.date;
- var heading =  data.heading;
-var htmltemplet = `
-<!DOCTYPE html>
-<html>
-   <head>
-    <title>${title}</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1"/>
-     <link href="/ui/style.css" rel="stylesheet" />
-    </head>
-     <body>
-        <div class="container">
-        <div>
-            <a href="/">Home</a>
-        </div>
-        <hr/>
-        <h1>
-           ${heading} 
-        </h1>
-        <div>
-           ${date}
-        </div>
-          ${contain}
-        </div>
-         </div>
-    </body>
-</html>
-`;
-return htmltemplet;
-}
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
-});
-
-app.get('/artical-one', function (req, res){
-     res.send(createTemplet(articalOne));
-});
-
-app.get('/artical-two', function (req, res){
-   res.sendFile(path.join(__dirname, 'ui', 'artical-two.html')); 
-});
-
-app.get('/artical-three', function (req, res){
-   res.sendFile(path.join(__dirname, 'ui', 'artical-three.html')); 
-});
-
-app.get('/artical-four', function (req, res){
-   res.sendFile(path.join(__dirname, 'ui', 'artical-four.html')); 
 });
 
 app.get('/ui/style.css', function (req, res) {
