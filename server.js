c0ebce192b15d3b90aaaf172a42092226ed8edcd
GopalJiGupta <gopaljigupta.17@gmail.com>
2017-02-10 15:28:52 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,22 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/artical-one', function (req, res){
+   res.send('this is artical-one responce Thank you!!'); 
+});
+
+app.get('/artical-two', function (req, res){
+   res.send('this is artical-Two responce Thank you!!'); 
+});
+
+app.get('/artical-three', function (req, res){
+   res.send('this is artical-Three responce Thank you!!'); 
+});
+
+app.get('/artical-four', function (req, res){
+   res.send('this is artical-Four responce Thank you!!'); 
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
