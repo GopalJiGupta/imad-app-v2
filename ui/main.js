e1a6fef66592d4af4345c91bb61502711015a8c2
GopalJiGupta <gopaljigupta.17@gmail.com>
2017-02-11 17:05:12 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,7 @@
 console.log('Loaded!');
+
+//change by server
+
+var element = document.getElementById('main-text');
+
+element.innertHTML = 'Hi! I am Gopal and i am a begginer of this course(nodejs). '<br>' this is a new code change by javascript ';
