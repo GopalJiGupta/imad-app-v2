9be36ccd509d5ca4a5a31bfb8342fd352bd47b79
GopalJiGupta <gopaljigupta.17@gmail.com>
2017-02-11 17:21:34 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -10,5 +10,5 @@ element.innerHTML = 'Hi! I am Gopal and i am a begginer of this course(nodejs). 
 
 var img = document.getElementById('jimg');
 img.onclick = function(){
-    img.style.marginleft='100px'; 
+    img.style.marginLeft= '100px'; 
 }; 
