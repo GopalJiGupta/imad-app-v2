6b61e536f1cc7384564519d5df01dd4f98800064
GopalJiGupta <gopaljigupta.17@gmail.com>
2017-02-12 20:16:30 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,21 +1,15 @@
 console.log('Loaded!');
 
-//html text change by JS
-
-var element = document.getElementById('main-text');
-
-element.innerHTML = 'Hi! I am Gopal and i am a begginer of this course(nodejs). this is a new code change by javascript ';
-
-//chaning Image
-
-var img = document.getElementById('jimg');
-var marginLeft = 0;
-function moveRight(){
-    marginLeft = marginLeft + 5;
-    img.style.marginLeft= marginLeft + 'px';
-}
-
-img.onclick = function(){
-    var interval = setInterval(moveRight,50);
-     
-}; 
+var button = Document.getElementById('counter');
+var counter = 0;
+button.onclick = function(){
+    
+    //make a request to the counter endpoint.
+    
+    //and capture the responce 
+    
+    //randering the variable in correct variable
+    counter = counter + 1;
+    var span = Document.getElementById('count');
+    span.innerHTML = counter.toString();
+};
