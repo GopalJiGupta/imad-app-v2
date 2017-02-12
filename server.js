ba9a51be6636fd66c12f7f8a859d520b84428cd0
GopalJiGupta <gopaljigupta.17@gmail.com>
2017-02-13 02:58:26 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -72,7 +72,6 @@ app.get('/counter', function (req, res) {
 
 var names = [];
 app.get('/submit-name', function (req, res){
-    
     //get name from the request
     var name= req.query.name;
     names.push(name);
