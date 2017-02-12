556311db65af6fb2b7b1e883187dc7acf190ccdc
GopalJiGupta <gopaljigupta.17@gmail.com>
2017-02-13 02:38:08 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -27,14 +27,28 @@ var nameinput = document.getElementById('name');
 var name = nameinput.value;
 var submitbt = document.getElementById('submit_bt');
  submitbt.onclick = function () {
-   //make a request to the server and send name
-   //caputure the list and rander with list
-   var names = ['name1','name2','name3','name4'];
-   var list = '';
-   for(var i=0; i<names.length; i++)
-   {
-       list += '<li>' + names[i] + '</li>';
-   }
-   var  ul = document.getElementById('namelist');
-   ul.innerHTML = list;
+    //create a request
+    var request = new XMLHttpRequest();
+    //and capture the responce 
+    request.onreadystatechange = function () {
+      if(request.readyState === XMLHttpRequest.DONE){
+          if(request.status === 200){
+             var names = request.responseText;
+             names = JSON.parse(names); 
+                 var list = '';
+              for(var i=0; i<names.length; i++)
+               {
+                  list += '<li>' + names[i] + '</li>';
+                }
+                   var  ul = document.getElementById('namelist');
+                    ul.innerHTML = list;
+          }
+      }  
+    };
+    //make the request 
+    request.open('GET', 'http://gopaljigupta.imad.hasura-app.io/submit-name?name=' + name, true);
+    request.send(null);
+};
+
+   
  };
