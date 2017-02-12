a3c9bc2c61315a6c4008f671cb793fa39dbcc03b
GopalJiGupta <gopaljigupta.17@gmail.com>
2017-02-13 02:02:34 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -31,7 +31,7 @@ var submitbt = document.getElementById('submit_bt');
    //caputure the list and rander with list
    var names = ['name1','name2','name3','name4'];
    var list = '';
-   for(var i =0; i<names.length; i++)
+   for(var i=0; i<names.length; i++)
    {
        list += '<li>' + names[i] + '</li>';
    }
