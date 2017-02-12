8d5155dbac11cc1a25b90ca40c73ea2d4fa1f0b8
GopalJiGupta <gopaljigupta.17@gmail.com>
2017-02-12 23:13:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -18,5 +18,5 @@ button.onclick = function(){
     };
     //make the request 
     request.open('GET', 'http//http://gopaljigupta.imad.hasura-app.io/counter', true);
-    request.send(null);
+    request.send();
 };
